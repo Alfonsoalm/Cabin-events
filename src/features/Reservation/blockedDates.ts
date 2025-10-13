4dfa78bd7cb65c949137ebdb3e2ec7c5514d5f67
Alfonso Almenara López <alfonsoalm34@gmail.com>
2025-10-13 19:54:16 +0200

    reserva de dia

--- src/features/Reservation/blockedDates.ts
+++ src/features/Reservation/blockedDates.ts
@@ -1,8 +1,24 @@
 export const blockedDates = [
-  new Date('2025-10-04'),
-  new Date('2025-10-05'),
-  new Date('2025-10-11'),
-  new Date('2025-10-26'),
-  new Date('2025-12-31'),
+  // 27 - 9 PAGINA WEB
+
+  new Date('2025-10-04'), // NO PAGADO
+  new Date('2025-10-05'), // NO PAGADO
+  new Date('2025-10-11'), // NO PAGADO
+  new Date('2025-10-13'),
+  new Date('2025-10-24'),// PAGINA WEB
+  // 25 - 10 // 
+  new Date('2025-10-26'), // PAGINA WEB
+  // 31 - 10
+
+  // 1 - 11
+  // 8 - 11 // PAGINA WEB
+  // 22 - 11
+
+  // 7 - 12 // DE DOMINGO
+  // 24 - 12 // DE OTRO AÑO
+  // 25 - 12 // DE OTRO AÑO
+  new Date('2025-12-31'),  
+
   new Date('2026-01-01'),
+  // 10 - 1
 ];
